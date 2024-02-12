fe24e87e1397125f4201601423ea80291905e90e
sbharani <sasikumar_bharanikumar@discovery.com>
2024-02-12 16:39:19 +0530

    Fixing html append issue

--- src/index.js
+++ src/index.js
@@ -48,6 +48,6 @@ router.get('/', function (req, res) {
 
   //add the router
   app.use('/', router);
-  app.listen(process.env.port || 3000);
+  app.listen(process.env.port || 3001);
    
-  console.log('Running at Port 3000');
+  console.log('Running at Port 3001');
